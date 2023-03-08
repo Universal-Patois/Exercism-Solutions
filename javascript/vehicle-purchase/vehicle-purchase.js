c509e3eaee2163f34daaf4dba39b3151bb81785a
Andrew Knapick <91920371+Universal-Patois@users.noreply.github.com>
2023-03-08 14:24:26 -0600

    Pass: Tests for needsLicense

---
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -0,0 +1,19 @@
+export function needsLicense(kind) {
+  return kind === 'car' || kind === 'truck';
+}
+
+export function chooseVehicle(option1, option2) {
+  t
+}
+
+/**
+ * Calculates an estimate for the price of a used vehicle in the dealership
+ * based on the original price and the age of the vehicle.
+ *
+ * @param {number} originalPrice
+ * @param {number} age
+ * @returns {number} expected resell price in the dealership
+ */
+export function calculateResellPrice(originalPrice, age) {
+  throw new Error('Please implement the calculateResellPrice function');
+}
