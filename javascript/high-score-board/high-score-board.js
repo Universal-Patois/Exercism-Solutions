329fba6e7c318dfb054dc384c27ba13db168be8a
Andrew Knapick <91920371+Universal-Patois@users.noreply.github.com>
2023-03-10 15:37:00 -0600

    Complete: High score board exercise

---
+++ javascript/high-score-board/high-score-board.js
@@ -0,0 +1,81 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new score board with an initial entry.
+ *
+ * @returns {Record<string, number>} new score board
+ */
+export function createScoreBoard() {
+  const scoreBoard = {
+    'The Best Ever': 1000000,
+  };
+  return scoreBoard;
+}
+
+/**
+ * Adds a player to a score board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} score
+ * @returns {Record<string, number>} updated score board
+ */
+export function addPlayer(scoreBoard, player, score) {
+  const newScoreBoard = scoreBoard;
+   scoreBoard[player] = score;
+   return newScoreBoard;
+}
+
+/**
+ * Removes a player from a score board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @returns {Record<string, number>} updated score board
+ */
+export function removePlayer(scoreBoard, player) {
+  const newScoreBoard = scoreBoard;
+  delete newScoreBoard[player];
+  return newScoreBoard;
+}
+
+/**
+ * Increases a player's score by the given amount.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} points
+ * @returns {Record<string, number>} updated score board
+ */
+export function updateScore(scoreBoard, player, points) {
+  const newScoreBoard = scoreBoard;
+  newScoreBoard[player] += points;
+  return newScoreBoard;
+}
+
+/**
+ * Applies 100 bonus points to all players on the board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @returns {Record<string, number>} updated score board
+ */
+export function applyMondayBonus(scoreBoard) {
+  const newScoreBoard = scoreBoard;
+  for (const player in newScoreBoard) {
+    newScoreBoard[player] += 100;
+  }
+  return newScoreBoard;
+}
+
+/**
+ * Normalizes a score with the provided normalization function.
+ *
+ * @param {Params} params the parameters for performing the normalization
+ * @returns {number} normalized score
+ */
+export function normalizeScore(params) {
+  const parameters = params;
+  return parameters.normalizeFunction(parameters.score);
+
+}
